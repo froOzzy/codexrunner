4eacc02bd82ef4b9b80da079541ce0fc73ff9a8d
vladislav <fro3zn@mail.ru>
2021-12-19 17:00:18 +0300

    Save last version

--- setup.py
+++ setup.py
@@ -4,7 +4,22 @@ from os.path import join, dirname
 setup(
     name='codexrunner',
     version='1.0',
-    packages=['codexrunner'],
+    packages=[
+        'codexrunner',
+        'codexrunner.management',
+        'codexrunner.management.commands',
+        'codexrunner.migrations',
+        'codexrunner.runner',
+        'codexrunner.runner.image',
+        'codexrunner.static',
+        'codexrunner.static.codexrunner',
+        'codexrunner.static.codexrunner.bootstrap5',
+        'codexrunner.static.codexrunner.codemirror',
+        'codexrunner.static.codexrunner.jquery',
+        'codexrunner.templates',
+        'codexrunner.templates.codexrunner',
+        'codexrunner.templatetags',
+    ],
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
         'docker>=5.0.3',
