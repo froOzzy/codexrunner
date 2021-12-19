c24c00db99ad11b7484ed69a7d014809180302ff
vladislav <fro3zn@mail.ru>
2021-12-19 17:37:02 +0300

    Save last version

--- codexrunner/management/commands/codexrunner_build_image.py
+++ codexrunner/management/commands/codexrunner_build_image.py
@@ -25,5 +25,4 @@ class Command(BaseCommand):
         if not image_name:
             image_name = DEFAULT_SETTINGS['IMAGE']['NAME']
 
-        subprocess.run(['cd', dockerfile_path])
-        subprocess.run(['docker', 'build', dockerfile_path, '--tag', image_name])
+        subprocess.run(['docker', 'build', dockerfile_path, '--tag', image_name], cwd=dockerfile_path)
