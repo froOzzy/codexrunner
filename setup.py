545813fad9fe678ee2b52c064d230f6124139789
froOzzy <53941414+froOzzy@users.noreply.github.com>
2022-01-12 11:51:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,9 +23,8 @@ setup(
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
         'docker>=5.0.3',
-        'Markdown>=3.3.6',
-        'django-rq>=2.5.1',
-        'docker>=5.0.3',
+        'Markdown>=3.3.4',
+        'django-rq>=2.4.1',
     ],
     include_package_data=True,
 )
